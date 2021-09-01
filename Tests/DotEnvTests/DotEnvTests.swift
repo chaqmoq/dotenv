95bf328973464273868395edb0c35f33bbf5a3c1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-01 14:08:40 +0500

    Test reading a non-existing file

--- Tests/DotEnvTests/DotEnvTests.swift
+++ Tests/DotEnvTests/DotEnvTests.swift
@@ -1,4 +1,26 @@
 @testable import DotEnv
 import XCTest
 
-final class DotEnvTests: XCTestCase {}
+final class DotEnvTests: XCTestCase {
+    let env = DotEnv()
+
+    override func setUp() {
+        super.setUp()
+
+        env.reset()
+    }
+
+    func testReadingNonExistingFile() {
+        // Arrange
+        let filePath = "/non-existing.env"
+
+        // Act/Assert
+        XCTAssertThrowsError(try env.readFile(at: filePath)) { error in
+            XCTAssertTrue(error is FileError)
+            XCTAssertEqual(error.localizedDescription, """
+            [File: "\(filePath)"] \(String(describing: FileError.self)): \(ErrorType.fileNotFound.message)
+            """
+            )
+        }
+    }
+}
