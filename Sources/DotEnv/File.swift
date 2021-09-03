0944b885b49d8fcb937486959e3f690ba6e27cc9
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-03 16:29:00 +0500

    Update `File` documentation

--- Sources/DotEnv/File.swift
+++ Sources/DotEnv/File.swift
@@ -1,12 +1,12 @@
 /// A representation of an environment file.
 public struct File: CustomStringConvertible, Equatable, ExpressibleByStringLiteral {
-    /// A content of a file.
+    /// A content of an environment file.
     public let source: String
 
-    /// An absolute path to a file in the file system. Defaults to `nil`.
+    /// An absolute path to an environment file in the file system. Defaults to `nil`.
     public let path: String?
 
-    /// See `ExpressibleByStringLiteral`.
+    /// See `CustomStringConvertible`.
     public var description: String { source }
 
     /// See `ExpressibleByStringLiteral`.
@@ -18,8 +18,8 @@ public struct File: CustomStringConvertible, Equatable, ExpressibleByStringLiter
     /// Initializes a new instance with the `source` and optionally `path` parameters.
     ///
     /// - Parameters:
-    ///   - source: A content of a file.
-    ///   - path: An absolute path to a file in the file system. Defaults to `nil`.
+    ///   - source: A content of an environment file.
+    ///   - path: An absolute path to an environment file in the file system. Defaults to `nil`.
     public init(_ source: String, path: String? = nil) {
         self.source = source
         self.path = path
