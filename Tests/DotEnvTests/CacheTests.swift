f24f6027dfd1abc737e8107511ad0d5aa1bdceb2
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-04 13:22:01 +0500

    Test `Cache` setting and getting

--- Tests/DotEnvTests/CacheTests.swift
+++ Tests/DotEnvTests/CacheTests.swift
@@ -22,4 +22,40 @@ final class CacheTests: XCTestCase {
         XCTAssertEqual(cache.costLimit, costLimit)
         XCTAssertEqual(cache.countLimit, countLimit)
     }
+
+    func testSetGet() {
+        // Arrange
+        let cache = Cache<String, String>()
+        let key1 = "key1"
+        let value1 = "value1"
+        let key2 = "key2"
+        let value2 = "value2"
+
+        // Act
+        cache.setValue(value1, forKey: key1)
+
+        // Assert
+        XCTAssertEqual(cache.getValue(forKey: key1), value1)
+
+        // Act
+        cache.setValue(value2, forKey: key2)
+
+        // Assert
+        XCTAssertEqual(cache.getValue(forKey: key1), value1)
+        XCTAssertEqual(cache.getValue(forKey: key2), value2)
+
+        // Act
+        cache.removeValue(forKey: key1)
+
+        // Assert
+        XCTAssertNil(cache.getValue(forKey: key1))
+        XCTAssertEqual(cache.getValue(forKey: key2), value2)
+
+        // Act
+        cache.clear()
+
+        // Assert
+        XCTAssertNil(cache.getValue(forKey: key1))
+        XCTAssertNil(cache.getValue(forKey: key2))
+    }
 }
