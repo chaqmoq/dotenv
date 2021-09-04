379efd53b308b9231bd4cb23c65705187f34a583
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-04 13:22:57 +0500

    Test `Cache` count limit

--- Tests/DotEnvTests/CacheTests.swift
+++ Tests/DotEnvTests/CacheTests.swift
@@ -81,5 +81,27 @@ final class CacheTests: XCTestCase {
         XCTAssertNil(cache.getValue(forKey: key1)) // FIXME: evicting key2 instead of key1 on Linux
         XCTAssertEqual(cache.getValue(forKey: key2), value2)
     }
+
+    func testCountLimit() {
+        // Arrange
+        let cache = Cache<String, String>(countLimit: 1)
+        let key1 = "key1"
+        let value1 = "value1"
+        let key2 = "key2"
+        let value2 = "value2"
+
+        // Act
+        cache.setValue(value1, forKey: key1)
+
+        // Assert
+        XCTAssertEqual(cache.getValue(forKey: key1), value1)
+
+        // Act
+        cache.setValue(value2, forKey: key2)
+
+        // Assert
+        XCTAssertNil(cache.getValue(forKey: key1)) // FIXME: evicting key2 instead of key1 on Linux
+        XCTAssertEqual(cache.getValue(forKey: key2), value2)
+    }
     #endif
 }
